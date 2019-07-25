fc47abb1a65e8cb640c30af6fb6aab897c833411
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-07-25 15:23:48 +0300

    bench:Update demoscripts

--- mpi-paper/demo_submit_devito_mpi_job.sh
+++ mpi-paper/demo_submit_devito_mpi_job.sh
@@ -2,12 +2,13 @@
 # Job name
 #PBS -N results_mpi_devito_job
 # Time required in hh:mm:ss
-#PBS -l walltime=00:05:00
+#PBS -l walltime=00:15:00
 # Resource requirements
-#PBS -lselect=4:ncpus=24:mem=46gb:mpiprocs=24:ompthreads=1
+#PBS -lselect=1:ncpus=24:mem=120gb:mpiprocs=12:ompthreads=2
 # Files to contain standard error and standard output
 
-echo Working Directory is $PBS_O_WORKDIR
+lscpu
+
 cd $PBS_O_WORKDIR
 
 echo -ne Loading required modules...
@@ -18,8 +19,9 @@ module load mpi
 mpicc -v
 echo ...DONE
 
+
 source activate devito
-conda install mpi4py
+#conda install mpi4py
 python print_mpi4py_conf.py
 
 cd $HOME/opesci/
@@ -29,15 +31,16 @@ export DEVITO_HOME=`pwd`/devito
 cd devito/benchmarks/user/
 
 # Start time
-echo Start time is `date` > date
+echo Start time is `date`
 
 # Set env variables
 export DEVITO_ARCH=intel
-export DEVITO_OPENMP=0
+export DEVITO_OPENMP=1
 export DEVITO_MPI=1
 export DEVITO_LOGGING=DEBUG
 export DEVITO_DEBUG_COMPILER=1
 # Experiment
-numactl --cpubind=0 --membind=0 mpiexec python benchmark.py bench -P acoustic -bm O2 -d 50 50 50 -so 12 -a --tn 60 --arch intel
+mpiexec -genv I_MPI_DEBUG=40 python benchmark.py bench -P acoustic -bm O2 -d 768 768 768 -so 12 --tn 100 --arch intel
+
 # End time
-echo End time is `date` >> date
+echo End time is `date`
